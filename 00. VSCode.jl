9e0d333afeac4a235bd8b995c56998c040bc68b5
Matt Bauman <mbauman@gmail.com>
2020-10-14 23:34:32 -0400

    Add README

--- 00. VSCode.jl
+++ 00. VSCode.jl
@@ -3,7 +3,7 @@
 # VS Code is a convenient way to view, edit, and display interactive code.
 #
 # # Installation:
-
+#
 # 1. Install Julia 1.5.2 for your platform: https://julialang.org/downloads/
 # 2. Install VS Code for your platform: https://code.visualstudio.com/download
 # 3. Launch VS Code!
@@ -11,7 +11,19 @@
 #       - executing the View: Show Extensions command (click View->Command Palette...)
 #       - or by clicking on the gear icon on the bottom left and selecting Extensions
 #   3.2 In the extensions view, simply search for "Julia" and install!
+#
+# # Getting these materials:
+#
+# These materials are hosted on GitHub at https://github.com/JuliaComputing/IntroductionTraining
+#
+# To download these materials, you can either:
+#
+#   - Download the zipped directory: https://github.com/JuliaComputing/IntroductionTraining/archive/master.zip
+#   - Use git to _clone_ the repository
+#       (I only recommend this if you are already familiar with git)
 
+# ----------------------------
+#
 # # Executing code
 # To execute a line of Julia code, place your cursor on that line and
 # and hit `Control` and `Enter`
